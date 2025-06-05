8dced8babb7844275111d05932f8d3b6c96e7404
Nisa Rianti <165540983+Nisarianti2218@users.noreply.github.com>
2025-06-05 16:59:02 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,20 @@
-from django.db import models
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
 
-class Kategori(models.Model):
-    nama = models.CharField(max_length=100)
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katalog.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-    def _str_(self):
-        return self.nama
-
-class Produk(models.Model):
-    nama = models.CharField(max_length=200)
-    harga = models.DecimalField(max_digits=10, decimal_places=2)
-    deskripsi = models.TextField()
-    gambar = models.CharField(max_length=200, blank=True)
-    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
-    tersedia = models.BooleanField(default=True)
-    spesifikasi = models.JSONField(default=list)
-
-    def _str_(self):
-        return self.nama
+if _name_ == '_main_':
+    main()
